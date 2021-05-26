ec63cda70a55be084330210383bc7feea54e603b
Robert Imschweiler <ro-i@posteo.de>
2021-05-26 02:00:05 +0200

    i3_bar.sh: Add parallel monitors.

--- i3_bar.sh
+++ i3_bar.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # See LICENSE file for copyright and license details.
 
+# The terminal emulator to use.
+terminal_app='gnome-terminal'
+
 declare -A colors
 colors=(
 	['default']='#e2e2e2'
@@ -22,6 +25,11 @@ line_items=(
 	'battery_f'
 )
 
+# Listen on external events triggering item updates.
+monitors=(
+	'volume_monitor'
+)
+
 # Which lineitems shall be updated every 10 seconds?
 # (The others are updated only on a click event.)
 update_items=(
@@ -33,7 +41,7 @@ update_items=(
 # The output line items (json objects).
 declare -A output_items
 
-# Regular update interval in seconds
+# Regular update interval in seconds.
 update_interval_default=10
 update_interval_short=5
 
@@ -41,6 +49,42 @@ update_interval_short=5
 regex_name='[^\\]"name":[[:space:]]*"(([^"]|\\")*[^\\])"'
 regex_button='[^\\]"button":[[:space:]]*([[:digit:]]+)'
 
+# Main input pipe. DO NOT CHANGE!
+fd_input_pipe=3
+
+
+## Utility functions
+## -----------------
+
+
+check_dependencies () {
+	dependencies=(
+		"$terminal_app" 'amixer' 'cal' 'cat' 'date' 'flock' 'less'
+		'pavucontrol' 'printf' 'stdbuf' 'tty'
+	)
+	for dep in "${dependencies[@]}"; do
+		type "$dep" > /dev/null 2>&1 && continue
+		printf '%s\n%s\n' "Error: unmet dependency: $dep" \
+			"My dependencies are: ${dependencies[*]}"
+		exit 1
+	done
+}
+
+# Update all items in the array passed as argument.
+do_update () {
+	local result
+
+	for func in "$@"; do
+		result=$("$func")
+		[[ -z $result ]] && result=$(err)
+
+		output_items["$func"]=$result
+	done
+}
+
+err () {
+	to_json 'err' 'error' "${colors['red']}"
+}
 
 # Executes an external command if it is available.
 # Takes the name of the command as first parameter and its arguments
@@ -61,6 +105,76 @@ exec_cmd () {
 	fi
 }
 
+# Takes a string and a regex with a capture and returns the captured
+# string, if it has been found.
+get_regex_match () {
+	[[ $1 =~ $2 ]]
+	printf '%s' "${BASH_REMATCH[1]}"
+}
+
+# Redirect input to $fd_input_pipe, using locking.
+input_redirector () {
+	# Get the pid of the actual bash process, not the subshell.
+	local fd="/proc/$$/fd/${fd_input_pipe}"
+
+	while read -d '}' -r input; do
+		# Append the delimiter again.
+		input+='}'
+		# Redirect the input to the actual input pipe.
+		flock "$fd" -c "printf '%s\n' '${input}' > '${fd}'"
+	done
+}
+
+# Takes an element and an array and checks if the array contains that
+# element.
+is_elem_of () {
+	local elem=$1
+	shift
+
+	for e in "$@"; do
+		[[ $e == "$elem" ]] && return 0
+	done
+
+	return 1
+}
+
+# Print a full status line. Returns an json array of json opdates
+# respresenting the line items.
+print_line () {
+	local delim result
+
+	for item in "${line_items[@]}"; do
+		result+="${delim}${output_items[$item]}"
+		[[ -z $delim ]] && delim=', '
+	done
+
+	printf '[%s],\n' "$result"
+}
+
+# A separate reader for stdin.
+# Avoids race conditions by using locking on the actual input pipe
+# which is also used by the monitors.
+start_input_listener () {
+	local fd=0
+
+	if tty -s; then
+		# Use a new file descriptor to not mess up with
+		# terminal stdin/stdout
+		exec 4<> <(:)
+		exec 0>>/proc/self/fd/4
+		fd=4
+	fi
+	(input_redirector < /proc/$$/fd/$fd &)
+}
+
+# Start all monitors in background and establish the input pipe
+# collecting their ouput.
+start_monitors () {
+	for monitor in "${monitors[@]}"; do
+		({ "$monitor" | input_redirector; } &)
+	done
+}
+
 # Gets the name of a lineitem, its new value and optionally also its
 # new color and returns a corresponding json object.
 to_json () {
@@ -74,10 +188,16 @@ to_json () {
 		"$name" "$full_text" "$color"
 }
 
+
+## Item update functions
+## ---------------------
+
+
 backlight_f () {
 	local button=$1
+	local brightness percentage
 
-	local brightness=$(< '/sys/class/backlight/intel_backlight/brightness')
+	brightness=$(< '/sys/class/backlight/intel_backlight/brightness')
 	[[ -z $brightness ]] && return
 
 	# Note: the brightness values here are very individual!
@@ -95,17 +215,17 @@ backlight_f () {
 			backlight_f ''
 			;;
 		*)
-			local percentage=$((brightness / 1200))
+			percentage=$((brightness / 1200))
 			to_json 'backlight_f' "☀ ${percentage}%"
 			;;
 	esac
 }
 
 battery_f () {
-	local suffix
+	local cap state suffix
 
-	local state=$(< '/sys/class/power_supply/BAT0/status')
-	local cap=$(< '/sys/class/power_supply/BAT0/capacity')
+	state=$(< '/sys/class/power_supply/BAT0/status')
+	cap=$(< '/sys/class/power_supply/BAT0/capacity')
 
 	[[ -z $state || -z $cap ]] && return
 
@@ -131,10 +251,10 @@ battery_f () {
 }
 
 ethernet_f () {
-	local result
 	local interface='eno2'
+	local result state
 
-	local state=$(< "/sys/class/net/${interface}/operstate")
+	state=$(< "/sys/class/net/${interface}/operstate")
 	[[ -z $state ]] && return
 
 	case $state in
@@ -159,7 +279,7 @@ time_f () {
 
 	case $button in
 		3)
-			exec_cmd 'gnome-terminal' '-- bash -c "cal -wy | less"' true
+			exec_cmd "$terminal_app" '-- bash -c "cal -wy | less"' true
 			;;
 		*)
 			to_json 'time_f' "$(date +'%H:%M - %d.%m')"
@@ -173,8 +293,7 @@ volume_intern () {
 	local device_alsa=$3
 	local device_character=$4
 	local device_character_muted=$5
-	local output
-	local result
+	local output result state vol
 
 	case $button in
 		2)
@@ -201,8 +320,8 @@ volume_intern () {
 
 	[[ -z $output ]] && { err; return; }
 
-	local vol=$(get_regex_match "$output" '([[:digit:]]+%)')
-	local state=$(get_regex_match "$output" '(\[on\]|\[off\])')
+	vol=$(get_regex_match "$output" '([[:digit:]]+%)')
+	state=$(get_regex_match "$output" '(\[on\]|\[off\])')
 
 	[[ -z $vol || -z $state ]] && return
 
@@ -233,19 +352,19 @@ volume_mic_f () {
 wifi_f () {
 	local button=$1
 	local interface='wlo1'
-	local result
+	local dbm operstate result wifi wireless
 
-	local operstate=$(< "/sys/class/net/${interface}/operstate")
+	operstate=$(< "/sys/class/net/${interface}/operstate")
 	[[ -z $operstate ]] && return
 
 	if [[ $operstate == 'down' ]]; then
 		to_json 'wifi_f' 'W down'
 	fi
 
-	local wireless=$(< '/proc/net/wireless')
+	wireless=$(< '/proc/net/wireless')
 	[[ -z $wireless ]] && return
 
-	local dbm=$(printf '%s' "$wireless" | \
+	dbm=$(printf '%s' "$wireless" | \
 		sed -nE '/^\s*'"${interface}"'/ { s/[^-]*-([0-9]*).*/\1/p;q }')
 	[[ -z $dbm ]] && return
 
@@ -254,65 +373,46 @@ wifi_f () {
 	elif ((dbm < 20)); then
 		result='100%'
 	else
-		local wifi=$((100 - ((dbm - 20)**2) / 53))
+		wifi=$((100 - ((dbm - 20)**2) / 53))
 		result="${wifi}%"
 	fi
 
 	to_json 'wifi_f' "$result"
 }
 
-err () {
-	to_json 'err' 'error' "${colors['red']}"
-}
 
-# Update all items in the array passed as argument.
-do_update () {
-	local result
+## Monitor functions
+## -----------------
 
-	for func in "$@"; do
-		result=$("$func")
-		[[ -z $result ]] && result=$(err)
 
-		output_items["$func"]=$result
-	done
-}
-
-# Takes a string and a regex with a capture and returns the captured
-# string, if it has been found.
-get_regex_match () {
-	[[ $1 =~ $2 ]]
-	printf '%s' "${BASH_REMATCH[1]}"
-}
-
-# Takes an element and an array and checks if the array contains that
-# element.
-is_elem_of () {
-	local elem=$1
-	shift
-
-	for e in "$@"; do
-		[[ $e == "$elem" ]] && return 0
-	done
-
-	return 1
-}
-
-# Print a full status line. Returns an json array of json opdates
-# respresenting the line items.
-print_line () {
-	local result
-	local delim
-
-	for item in "${line_items[@]}"; do
-		result+="${delim}${output_items[$item]}"
-		[[ -z $delim ]] && delim=', '
-	done
-
-	printf '[%s],\n' "$result"
+# Do not update the volume using a fixed interval, but monitor volume
+# changes (on all cards).
+volume_monitor () {
+	while read -r line; do
+		if [[ "$line" == *' Capture '* ]]; then
+			name='volume_mic_f'
+		elif [[ "$line" == *' Playback '* ]]; then
+			name='volume_f'
+		else
+			continue
+		fi
+		printf '%s' "{\"name\": \"${name}\", \"button\": 1}"
+	done < <(stdbuf --output=L alsactl monitor pulse)
 }
 
 
-update_interval=$update_interval_default
+## Main part
+## ---------
+
+
+# Check for dependencies.
+check_dependencies
+
+# Establish main pipe using fd_input_pipe.
+exec 3<> <(:)
+
+start_input_listener
+start_monitors
 
 # Print header and start infinite array.
 # - cf. https://i3wm.org/docs/i3bar-protocol.html
@@ -322,13 +422,14 @@ printf '%s\n%s\n' '{ "version": 1, "click_events": true }' '['
 do_update "${line_items[@]}"
 print_line
 
+update_interval=$update_interval_default
 # Initialize time counter.
 last_time=$EPOCHSECONDS
 
 while true; do
 	# Wait for input until we need to update.
 	# Read until we encounter the end of the json array.
-	if ! read -d '}' -r -t $update_interval input; then
+	if ! read -d '}' -r -t $update_interval -u 3 input; then
 		# Prevent infinite loops.
 		((last_time == EPOCHSECONDS)) && exit 1
 		last_time=$EPOCHSECONDS
