219e4213fe4b198eae304e34525c05d427fabbca
Robert Imschweiler <ro-i@posteo.de>
2025-02-10 09:34:46 +0100

    update nvim install script

--- nvim_install.sh
+++ nvim_install.sh
@@ -14,20 +14,20 @@ shell_file="$HOME/.${shell}rc"
 files_target_dir="$HOME/.local"
 
 nvim_release="nightly" # "v0.9.4"
-nvim_file="nvim-linux64.tar.gz"
+nvim_file="nvim-linux-x86_64.tar.gz"
 nvim_dir="${nvim_file%.tar.gz}"
-nvim_checksum_file="nvim-linux64.tar.gz.sha256sum"
+nvim_checksum_file="shasum.txt"
 
-rg_release="13.0.0"
+rg_release="14.1.1"
 rg_file="ripgrep-${rg_release}-x86_64-unknown-linux-musl.tar.gz"
 rg_dir="${rg_file%.tar.gz}"
 
-fd_release="v8.7.1"
+fd_release="v10.2.0"
 fd_file="fd-${fd_release}-x86_64-unknown-linux-gnu.tar.gz"
 fd_dir="${fd_file%.tar.gz}"
 
-nvm_url="https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh"
-node_release="20"
+#nvm_url="https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh"
+#node_release="20"
 
 
 while getopts "hi:s:" opt; do
@@ -50,7 +50,6 @@ while getopts "hi:s:" opt; do
     esac
 done
 
-
 # Create necessary distribution target directories in $HOME if they don't already exist.
 for directory in bin lib man share; do
     mkdir -p "$files_target_dir/$directory"
@@ -67,9 +66,11 @@ cd "$install_dir"
 
 # Install nvim.
 # Download nvim file, overwriting potentially existing file.
+echo "Retrieving ${nvim_file} ..."
 wget -qO "${nvim_file}" "https://github.com/neovim/neovim/releases/download/${nvim_release}/${nvim_file}"
+echo "Retrieving ${nvim_checksum_file} ..."
 wget -qO "${nvim_checksum_file}" "https://github.com/neovim/neovim/releases/download/${nvim_release}/${nvim_checksum_file}"
-if ! sha256sum -c "$nvim_checksum_file"; then
+if ! grep "$nvim_file" "$nvim_checksum_file" | sha256sum -c; then
     echo "ERROR: checksum mismatch"
     exit 1
 fi
@@ -92,6 +93,7 @@ done
 
 # Install ripgrep.
 # Download rg file, overwriting potentially existing file.
+echo "Retrieving ${rg_file} ..."
 wget -qO "${rg_file}" "https://github.com/BurntSushi/ripgrep/releases/download/${rg_release}/${rg_file}"
 # Remove potentially existing rg dir.
 rm -rf "$rg_dir"
@@ -102,6 +104,7 @@ cp "$install_dir/$rg_dir/doc/rg.1" "$files_target_dir/man/man1/"
 
 # Install fd.
 # Download fg file, overwriting potentially existing file.
+echo "Retrieving ${fd_file} ..."
 wget -qO "${fd_file}" "https://github.com/sharkdp/fd/releases/download/${fd_release}/${fd_file}"
 # Remove potentially existing fd dir.
 rm -rf "$fd_dir"
@@ -112,12 +115,12 @@ cp "$install_dir/$fd_dir/fd.1" "$files_target_dir/man/man1/"
 
 # install npm/node (required for nvim LSP support).
 # The nvm script seems to require bash to be executed.
-PROFILE=/dev/null bash -c "wget -qO- ${nvm_url} | bash"
-if ! grep -q NVM_DIR "$shell_file"; then
-    echo 'export NVM_DIR="$HOME/.nvm"' >> "$shell_file"
-    echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" --no-use  # This loads nvm' >> "$shell_file"
-fi
-"${shell}" -c ". ${shell_file} && . ${NVM_DIR}/nvm.sh && nvm install ${node_release}"
+#PROFILE=/dev/null bash -c "wget -qO- ${nvm_url} | bash"
+#if ! grep -q NVM_DIR "$shell_file"; then
+#    echo 'export NVM_DIR="$HOME/.nvm"' >> "$shell_file"
+#    echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" --no-use  # This loads nvm' >> "$shell_file"
+#fi
+#"${shell}" -c ". ${shell_file} && . ${NVM_DIR}/nvm.sh && nvm install ${node_release}"
 
 # Install neovim python module.
 pip3 install -U pynvim
